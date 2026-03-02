1ce1c01674219af11a40d2aa31a85b9240cc5eeb
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-01 20:33:36 -0600

    Update watch_community_by_id.dart

--- lib/src/domain/use_cases/communities/watch_community_by_id.dart
+++ lib/src/domain/use_cases/communities/watch_community_by_id.dart
@@ -8,20 +8,18 @@ class WatchCommunityById {
   final MembershipDataSource _membershipDataSource = GetIt.I<MembershipDataSource>();
   final PaymentDataSource _paymentDataSource = GetIt.I<PaymentDataSource>();
   final PropertyDataSource _propertyDataSource = GetIt.I<PropertyDataSource>();
-  final UserDataSource _userDataSource = GetIt.I<UserDataSource>();
 
   final GetCommunityById _getCommunityById = GetCommunityById();
 
   Stream<CommunityEntity> call(String id) {
-    return CombineLatestStream.combine6(
+    return CombineLatestStream.combine5(
       _applicationDataSource.watchByCommunityId(id),
       _communitySource.watchById(id),
       _membershipDataSource.watchByCommunityId(id),
       _paymentDataSource.watchByCommunityId(id),
       _propertyDataSource.watchByCommunityId(id),
-      _userDataSource.watchByCommunityId(id),
 
-      (applications, community, memebrships, payments, properties, users) {
+      (applications, community, memberships, payments, properties) {
         final community = _getCommunityById.call(id);
         return community;
       },
