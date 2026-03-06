8c7a0f22a0c32e85d1724afd33ccd1355aedafec
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-06 16:40:42 -0600

    Update create_application_and_send_invitations.dart

--- lib/src/domain/use_cases/applications/create_application_and_send_invitations.dart
+++ lib/src/domain/use_cases/applications/create_application_and_send_invitations.dart
@@ -1,10 +1,7 @@
-import 'package:get_it/get_it.dart';
 import 'package:resipal_core/lib.dart';
-import 'package:resipal_core/src/domain/use_cases/applications/create_application.dart';
 
 class CreateApplicationAndSendInvitations {
   Future call(CreateApplicationDto dto) async {
-    // TODO: Do some validation of the DTO
     await CreateApplication().call(dto);
     await SendInvitationEmail().call(
       email: dto.email,
