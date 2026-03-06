080c8fda64ef059079e54629df09968210465fd2
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-06 08:37:42 -0600

    Updates

--- lib/src/data/sources/email_invitation_data_source.dart
+++ lib/src/data/sources/email_invitation_data_source.dart
@@ -5,12 +5,12 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class EmailInvitationDataSource {
   final LoggerService _logger = GetIt.I<LoggerService>();
-  final SupabaseClient _client = GetIt.I<SupabaseClient>();
+  final ResipalSupabase _resipalSupabase = GetIt.I<ResipalSupabase>();
 
   final Map<String, EmailInvitationModel> _cache = {};
 
   Stream<EmailInvitationModel> watchById(String id) {
-    return _client.from('email_invitations').stream(primaryKey: ['id']).eq('id', id).map((data) {
+    return _resipalSupabase.client.from('email_invitations').stream(primaryKey: ['id']).eq('id', id).map((data) {
       if (data.isEmpty) throw Exception('Invitation not found');
       final model = EmailInvitationModel.fromMap(data.first);
       _cache[model.id] = model;
@@ -19,7 +19,7 @@ class EmailInvitationDataSource {
   }
 
   Stream<List<EmailInvitationModel>> watchByCommunityId(String communityId) {
-    return _client
+    return _resipalSupabase.client
         .from('email_invitations')
         .stream(primaryKey: ['id'])
         .eq('community_id', communityId)
@@ -35,7 +35,7 @@ class EmailInvitationDataSource {
   EmailInvitationModel? getById(String id) => _cache[id];
 
   Future fetchAndCacheByCommunityId(String communityId) async {
-    final items = await _client.from('email_invitations').select().eq('community_id', communityId);
+    final items = await _resipalSupabase.client.from('email_invitations').select().eq('community_id', communityId);
     for (var item in items) {
       final model = EmailInvitationModel.fromMap(item);
       _cache[model.id] = model;
@@ -43,24 +43,15 @@ class EmailInvitationDataSource {
   }
 
   Future upsert(Map<String, dynamic> map) async {
-    await _client.from('email_invitations').upsert(map);
+    await _resipalSupabase.client.from('email_invitations').upsert(map);
   }
 
-  Future<void> invokeSendInvitationEmail({
-    required String bearerKey,
-    required String email, required String name, required String message}) async {
-    
-    await _client.functions.invoke(
+  Future<void> invokeSendInvitationEmail({required String email, required String name, required String message}) async {
+    await _resipalSupabase.client.functions.invoke(
       'send_invitation_via_email',
       body: {
         'record': {'email': email, 'name': name, 'message': message},
       },
-      headers: {
-
-         "Content-Type": "application/json",
-         "Authorization": "Bearer ${bearerKey}"
-         
-         }
     );
   }
 }
