03546012cec66b5bfa65ce61cb2216de33b933db
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-10 13:55:36 -0600

    Update watch_property_by_id.dart

--- lib/src/domain/use_cases/properties/watch_property_by_id.dart
+++ lib/src/domain/use_cases/properties/watch_property_by_id.dart
@@ -1,16 +1,23 @@
 import 'package:get_it/get_it.dart';
+import 'package:resipal_core/lib.dart';
 import 'package:resipal_core/src/data/sources/property_data_source.dart';
 import 'package:resipal_core/src/domain/entities/property_entity.dart';
 import 'package:resipal_core/src/domain/use_cases/properties/get_property_by_id.dart';
+import 'package:rxdart/rxdart.dart';
 
 class WatchPropertyById {
-  final PropertyDataSource _source = GetIt.I<PropertyDataSource>();
+  final PropertyDataSource _propertyDataSource = GetIt.I<PropertyDataSource>();
+  final MaintenanceFeeDataSource _maintenanceFeeDataSource = GetIt.I<MaintenanceFeeDataSource>();
   final GetPropertyById _getProperty = GetPropertyById();
 
   Stream<PropertyEntity> call(String id) {
-    return _source.watchById(id).map((model) {
-      final entity = _getProperty.call(model.id);
-      return entity;
-    });
+    return CombineLatestStream.combine2(
+      _propertyDataSource.watchById(id),
+      _maintenanceFeeDataSource.watchByPropertyId(id),
+      (property, fees) {
+        final property = _getProperty.call(id);
+        return property;
+      },
+    ).distinct();
   }
 }
