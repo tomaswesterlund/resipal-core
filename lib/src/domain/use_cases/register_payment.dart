d391a99f3450cef65a2def48da10f92a00f6e44f
Tomas Westerlund <tomaswesterlund@gmail.com>
2026-03-10 08:51:56 -0600

    Update register_payment.dart

--- lib/src/domain/use_cases/register_payment.dart
+++ lib/src/domain/use_cases/register_payment.dart
@@ -1,12 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:resipal_core/lib.dart';
-import 'package:resipal_core/src/data/sources/payment_data_source.dart';
-import 'package:resipal_core/src/domain/use_cases/users/get_signed_in_user.dart';
 
 class RegisterPayment {
+  final LoggerService _logger = GetIt.I<LoggerService>();
   final PaymentDataSource _source = GetIt.I<PaymentDataSource>();
 
-  Future call({
+  Future<void> call({
     required String communityId,
     required String userId,
     required int amountInCents,
@@ -15,22 +14,45 @@ class RegisterPayment {
     required String? note,
     required String receiptPath,
   }) async {
-    final member = GetMemberByUserAndCommunityId().call(communityId: communityId, userId: userId);
-    final user = GetSignedInUser().call();
+    const String featureArea = 'RegisterPayment';
 
-    if(user.id != userId && member.isAdmin == false) {
-      throw Exception('User not allowed to create payment.');
+    try {
+      _logger.info('[$featureArea] Starting payment registration for user: $userId in community: $communityId');
+
+      final member = GetMemberByUserAndCommunityId().call(communityId: communityId, userId: userId);
+      final user = GetSignedInUser().call();
+
+      if (user.id != userId && member.isAdmin == false) {
+        final error = 'User ${user.id} not allowed to create payment for $userId.';
+        _logger.debug('[$featureArea] Authorization failed: $error');
+        throw Exception(error);
+      }
+
+      await _source.registerPayment(
+        communityId: communityId,
+        userId: userId,
+        amountInCents: amountInCents,
+        date: date,
+        reference: reference,
+        note: note,
+        receiptPath: receiptPath,
+      );
+
+      _logger.info('[$featureArea] Payment successfully registered. Amount: $amountInCents cents');
+    } catch (e, stackTrace) {
+      await _logger.logException(
+        featureArea: featureArea,
+        exception: e,
+        stackTrace: stackTrace,
+        metadata: {
+          'community_id': communityId,
+          'target_user_id': userId,
+          'amount': amountInCents,
+          'has_receipt': receiptPath.isNotEmpty,
+        },
+      );
+
+      rethrow;
     }
-    
-
-    await _source.registerPayment(
-      communityId: communityId,
-      userId: userId,
-      amountInCents: amountInCents,
-      date: date,
-      reference: reference,
-      note: note,
-      receiptPath: receiptPath,
-    );
   }
 }
